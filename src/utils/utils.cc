a09e57584d7cace47b408d47ce3314e0166bec14
zhangyunze <zhangyunze@qiyuanlab.com>
2025-12-30 06:17:09 +0000

    feat: insert ElementWise Op backend

--- src/utils/utils.cc
+++ src/utils/utils.cc
@@ -1,32 +1,34 @@
 #include "utils/utils.h"
 
 namespace infini {
-Shape infer_broadcast(const Shape &A, const Shape &B) {
-    if (A.empty() && B.empty()) {
-        return {};
+ShapeExpr infer_broadcast(const ShapeExpr &A, const ShapeExpr &B) {
+    if (A->size() == 0 && B->size() == 0) {
+        return ShapeExpr({});
     }
     auto A_ = A;
     auto B_ = B;
-    int rankA = A.size();
-    int rankB = B.size();
-    int rank = std::max(rankA, rankB);
+    size_t rankA = A->size();
+    size_t rankB = B->size();
+    size_t rank = std::max(rankA, rankB);
     if (rankA < rank) {
-        for (int i = 0; i < rank - rankA; ++i) {
-            A_.insert(A_.begin(), 1);
+        for (size_t i = 0; i < rank - rankA; ++i) {
+            A_->insert(0, ExprObj::constant(1));
         }
     }
     if (rankB < rank) {
-        for (int i = 0; i < rank - rankB; ++i) {
-            B_.insert(B_.begin(), 1);
+        for (size_t i = 0; i < rank - rankB; ++i) {
+            B_->insert(0, ExprObj::constant(1));
         }
     }
-    Shape ret;
-    for (int i = 0; i < rank; ++i) {
-        IT_ASSERT(A_[i] == B_[i] || A_[i] == 1 || B_[i] == 1);
-        auto shapeEle = std::max(A_[i], B_[i]);
-        ret.emplace_back(shapeEle);
+    // 逐维度计算广播结果
+    std::vector<Expr> resultDims;
+    for (size_t i = 0; i < rank; ++i) {
+        IT_ASSERT((*A_)[i] == (*B_)[i] || (*A_)[i] == ExprObj::constant(1) ||
+                  (*B_)[i] == ExprObj::constant(1));
+        auto shapeEle = (*A_)[i] == ExprObj::constant(1) ? (*B_)[i] : (*A_)[i];
+        resultDims.emplace_back(shapeEle);
     }
-    return ret;
+    return make_ref<ShapeExprObj>(ShapeExprObj(resultDims));
 }
 
 size_t calculateLinearOffset(size_t index, Shape shape, Stride stride) {
